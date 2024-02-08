1b63f0e8585c7de2fa61b709d578ec9a7dfbafb6
Sapfire65 <cot65@yandex.ru>
2024-02-09 00:26:12 +0300

    создание метода generate_fake_email

--- base_metods.py
+++ base_metods.py
@@ -1,8 +1,17 @@
 import requests
+from faker import Faker
 
 class Base:
     """Базовые методы для всего проекта"""
 
-    def generate_fake_email(self):
-        ...
+    def generate_fake_email():
+        fake = Faker()
+
+        email = fake.email()
+        print(email)
+
+
+
+Base.generate_fake_email()
+
 
